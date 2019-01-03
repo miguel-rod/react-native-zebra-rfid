324f3f8fe265c8db4c5e838ed65bef9693d4adb4
Homan Lui <Homan@tl.com>
2019-01-03 11:38:45 +0800

    i

--- android/src/main/java/com/headuck/reactnativezebrarfid/RFIDScannerThread.java
+++ android/src/main/java/com/headuck/reactnativezebrarfid/RFIDScannerThread.java
@@ -1,6 +1,7 @@
 package com.headuck.reactnativezebrarfid;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -253,7 +254,136 @@ public abstract class RFIDScannerThread extends Thread implements RfidEventsList
         }
         tempDisconnected = false;
         reading = false;
-        this.connect();
+        new connect().execute();
+        // this.connect();
+    }
+
+    class connect extends AsyncTask<Object, Integer, Integer> {
+        protected Integer doInBackground(Object... params) {
+            // do background work
+            String err = null;
+            if (rfidReaderDevice != null) {
+                if (rfidReaderDevice.getRFIDReader().isConnected())
+                disconnect();
+                return 1;
+            }
+            try {
+
+                Log.v("RFID", "initScanner");
+
+                ArrayList<ReaderDevice> availableRFIDReaderList = null;
+                try {
+                    availableRFIDReaderList = readers.GetAvailableRFIDReaderList();
+                    Log.v("RFID", "Available number of reader : " + availableRFIDReaderList.size());
+                    deviceList = availableRFIDReaderList;
+
+                } catch (InvalidUsageException e) {
+                    Log.e("RFID", "Init scanner error - invalid message: " + e.getMessage());
+                } catch (NullPointerException ex) {
+                    Log.e("RFID", "Blue tooth not support on device");
+                }
+
+                int listSize = (availableRFIDReaderList == null) ? 0 : availableRFIDReaderList.size();
+                Log.v("RFID", "Available number of reader : " + listSize);
+
+                if (listSize > 0) {
+                    ReaderDevice readerDevice = availableRFIDReaderList.get(0);
+                    RFIDReader rfidReader = readerDevice.getRFIDReader();
+                    // Connect to RFID reader
+                    Log.v("RFID", "Available rfid reader : " + rfidReader);
+                    if (rfidReader != null) {
+                        while (true) {
+                            try {
+                                rfidReader.connect();
+                                rfidReader.Config.getDeviceStatus(true, false, false);
+                                // rfidReader.Events.addEventsListener(this);
+                                // Subscribe required status notification
+                                rfidReader.Events.setInventoryStartEvent(true);
+                                rfidReader.Events.setInventoryStopEvent(true);
+                                // enables tag read notification
+                                rfidReader.Events.setTagReadEvent(true);
+                                rfidReader.Events.setReaderDisconnectEvent(true);
+                                rfidReader.Events.setBatteryEvent(true);
+                                rfidReader.Events.setBatchModeEvent(true);
+                                rfidReader.Events.setHandheldEvent(true);
+                                // Set trigger mode
+                                setTriggerImmediate(rfidReader);
+                                break;
+                            } catch (OperationFailureException ex) {
+                                if (ex.getResults() == RFIDResults.RFID_READER_REGION_NOT_CONFIGURED) {
+                                    // Get and Set regulatory configuration settings
+                                    try {
+                                        RegulatoryConfig regulatoryConfig = rfidReader.Config.getRegulatoryConfig();
+                                        SupportedRegions regions = rfidReader.ReaderCapabilities.SupportedRegions;
+                                        int len = regions.length();
+                                        boolean regionSet = false;
+                                        for (int i = 0; i < len; i++) {
+                                            RegionInfo regionInfo = regions.getRegionInfo(i);
+                                            if ("HKG".equals(regionInfo.getRegionCode())) {
+                                                regulatoryConfig.setRegion(regionInfo.getRegionCode());
+                                                rfidReader.Config.setRegulatoryConfig(regulatoryConfig);
+                                                Log.i("RFID", "Region set to " + regionInfo.getName());
+                                                regionSet = true;
+                                                break;
+                                            }
+                                        }
+                                        if (!regionSet) {
+                                            err = "Region not found";
+                                            break;
+                                        }
+                                    } catch (OperationFailureException ex1) {
+                                        err = "Error setting RFID region: " + ex1.getMessage();
+                                        break;
+                                    }
+                                } else if (ex.getResults() == RFIDResults.RFID_CONNECTION_PASSWORD_ERROR) {
+                                    // Password error
+                                    err = "Password error";
+                                    break;
+                                } else if (ex.getResults() == RFIDResults.RFID_BATCHMODE_IN_PROGRESS) {
+                                    // handle batch mode related stuff
+                                    err = "Batch mode in progress";
+                                    break;
+                                } else {
+                                    err = ex.getResults().toString();
+                                    break;
+                                }
+                            } catch (InvalidUsageException e1) {
+                                Log.e("RFID", "InvalidUsageException: " + e1.getMessage() + " " + e1.getInfo());
+                                err = "Invalid usage " + e1.getMessage();
+                                break;
+                            }
+                        }
+                    } else {
+                        err = "Cannot get rfid reader";
+                    }
+                    if (err == null) {
+                        // Connect success
+                        rfidReaderDevice = readerDevice;
+                        tempDisconnected = false;
+                        WritableMap event = Arguments.createMap();
+                        event.putString("RFIDStatusEvent", "opened");
+                        Log.i("RFID", "Connected to " + rfidReaderDevice.getName());
+
+                    }
+                } else {
+                    err = "No connected device";
+                }
+            } catch (InvalidUsageException e) {
+                err = "connect: invalid usage error: " + e.getMessage();
+            }
+            if (err != null) {
+                Log.e("RFID", err);
+            }
+            return 1;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            // optionally report progress
+        }
+
+        protected void onPostExecute(Integer result) {
+            // do something on the UI thread
+        }
     }
 
     public void shutdown() {
